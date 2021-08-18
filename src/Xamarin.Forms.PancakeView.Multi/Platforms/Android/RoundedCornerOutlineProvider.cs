4d34621b42d627b4fff15b4052ded16ddf117f9f
Solomon Mauskopf <zalke@pcwoodfloors.com>
2021-08-18 14:03:14 -0400

    small bug

--- src/Xamarin.Forms.PancakeView.Multi/Platforms/Android/RoundedCornerOutlineProvider.cs
+++ src/Xamarin.Forms.PancakeView.Multi/Platforms/Android/RoundedCornerOutlineProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Android.Graphics;
+using Android.OS;
 using Android.Views;
 
 namespace Xamarin.Forms.PancakeView.Droid
@@ -15,15 +16,24 @@ namespace Xamarin.Forms.PancakeView.Droid
             _convertToPixels = convertToPixels;
         }
 
+        [Obsolete]
+#pragma warning disable CS0809 // Obsolete member overrides non-obsolete member
         public override void GetOutline(global::Android.Views.View view, Outline outline)
+#pragma warning restore CS0809 // Obsolete member overrides non-obsolete member
         {
             try
             {
                 if (_pancake.Sides != 4)
                 {
                     var hexPath = DrawingExtensions.CreatePolygonPath(view.Width, view.Height, _pancake.Sides, _pancake.Shadow != null ? 0 : _pancake.CornerRadius.TopLeft, _pancake.OffsetAngle);
-
-                    outline.SetPath(hexPath);
+                    if (Build.VERSION.SdkInt >= BuildVersionCodes.R)
+                    {
+                        outline.SetPath(hexPath);
+                    }
+                    else if (hexPath.IsConvex)
+                    {
+                        outline.SetConvexPath(hexPath);
+                    }
                 }
                 else
                 {
@@ -32,8 +42,14 @@ namespace Xamarin.Forms.PancakeView.Droid
                         _convertToPixels(_pancake.CornerRadius.TopRight),
                         _convertToPixels(_pancake.CornerRadius.BottomRight),
                         _convertToPixels(_pancake.CornerRadius.BottomLeft));
-
-                    outline.SetPath(path);
+                    if (Build.VERSION.SdkInt >= BuildVersionCodes.R)
+                    {
+                        outline.SetPath(path);
+                    }
+                    else if (path.IsConvex)
+                    {
+                        outline.SetConvexPath(path);
+                    }
                 }
             }
             catch (Exception ex)
--- src/Xamarin.Forms.PancakeView.Multi/Platforms/Shared/GradientStopCollection.cs
+++ src/Xamarin.Forms.PancakeView.Multi/Platforms/Shared/GradientStopCollection.cs
@@ -1,3 +1,4 @@
+using PropertyChanged;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -14,28 +15,41 @@ namespace Xamarin.Forms.PancakeView
         protected override void ClearItems()
         {
             foreach (var item in this)
+            {
                 if (item is INotifyPropertyChanged i)
+                {
                     i.PropertyChanged -= CollectionItem_PropertyChanged;
+                }
+            }
 
             var removed = new List<GradientStop>(this);
             base.ClearItems();
             base.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, removed));
         }
 
+        [SuppressPropertyChangedWarnings]
         protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
         {
             if (e.OldItems != null)
+            {
                 foreach (INotifyPropertyChanged item in e.OldItems)
+                {
                     item.PropertyChanged -= CollectionItem_PropertyChanged;
+                }
+            }
 
             if (e.NewItems != null)
+            {
                 foreach (INotifyPropertyChanged item in e.NewItems)
+                {
                     item.PropertyChanged += CollectionItem_PropertyChanged;
+                }
+            }
 
             base.OnCollectionChanged(e);
         }
 
-        void CollectionItem_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        private void CollectionItem_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
             OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset, null));
         }
